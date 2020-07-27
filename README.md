0aa98698658fa86b38b64eeb464923df725ebb0e
Luciano Junior <engluciano.junior@gmail.com>
2020-07-27 00:44:42 -0300

    exibir tabelas

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Cadastro feito em Python com Qt Designer
 #####
 CRIAR O BANCO E AS TABELAS NO MYSQL
 
+###########################################################
+Implementando um sistema de gestão de cliente com cadastro listagem e exclusão
--- controle.py
+++ controle.py
@@ -8,38 +8,64 @@ banco = mysql.connector.connect(
     passwd="123456",
     database="cadastro_python"
 )
-print(banco)
 
+print(banco)
 
 
 def funcao_principal():
 
     campo1 = Projeto_Formulario.lineEdit.text()
-    campo2 = Projeto_Formulario.lineEdit.text()
+    campo2 = Projeto_Formulario.lineEdit_2.text()
     campo3 = Projeto_Formulario.lineEdit_3.text()
+
+    categoria = ""
 
     if Projeto_Formulario.radioButton.isChecked():
         print("Categoria Informática ")
+        categoria = "Informática"
     elif Projeto_Formulario.radioButton_2.isChecked():
         print("Categoria Alimentos")
+        categoria = "Alimentos"
     else :
         print("Categoria Eletrônicos ")
-
-    
-
+        categoria = "Eletrônicos"
 
     print("Codigo", campo1)
     print("Descricao", campo2)
     print("Preco", campo3)
-
     
+    cursor = banco.cursor()
+    comando_SQL = "INSERT INTO produtos (codigo,descriçao,preco,categoria) VALUES (%s,%s,%s,%s)"
+    dados = (str(campo1),str(campo2),str(campo3),categoria)
+    cursor.execute(comando_SQL,dados)
+    banco.commit()
+    Projeto_Formulario.lineEdit.setText("")
+    Projeto_Formulario.lineEdit_2.setText("")
+    Projeto_Formulario.lineEdit_3.setText("")
 
+def Chama_segundatela():
+    Projeto_Formulario2.show()
+    cursor = banco.cursor()
+    comando_SQL = "SELECT * FROM produtos"
+    cursor.execute(comando_SQL)
+    dados_lidos = cursor.fetchall()
 
+########exibi as colunas######################
+
+    Projeto_Formulario2.tableWidget.setRowCount(len(dados_lidos))
+    Projeto_Formulario2.tableWidget.setColumnCount(5)
+
+    for i in range(0, len(dados_lidos)):
+        for j in range(0,5):
+            Projeto_Formulario2.tableWidget.setItem(i,j,QtWidgets.QTableWidgetItem(str(dados_lidos[i][j])))
 
 
 app=QtWidgets.QApplication([])
 Projeto_Formulario=uic.loadUi("Projeto_Formulario.ui")
+Projeto_Formulario2=uic.loadUi("Projeto_Formulario2.ui")
+
 Projeto_Formulario.pushButton.clicked.connect(funcao_principal)
+Projeto_Formulario.pushButton_2.clicked.connect(Chama_segundatela)
 
 Projeto_Formulario.show()
 app.exec()
